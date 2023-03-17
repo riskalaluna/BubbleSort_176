288640da234f0efc249ffd91f253ab32c2444840
riskalaluna <riskalaluna@gmail.com>
2023-03-17 19:22:07 +0700

    Menghapus pemograman yang tidak digunakan

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -1,13 +1,3 @@
-// BubbleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
 #include <iostream>
 using namespace std;
 
